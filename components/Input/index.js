910b03b1040893c50e2a8aea871bbec291beafd2
Huey <leejinhuey@gmail.com>
2019-08-19 00:02:40 +0800

    mitigated #169, but will not fix altogether at this time because it is probably not worth the effort to adjust the backgroundColor of the inner Horizontal element onPressIn and changing it back onPressOut

--- components/Input/index.js
+++ components/Input/index.js
@@ -1,3 +1,3 @@
-export { default as TextInput } from "./TextInput";
+export { default as TextInput } from "./TextInput"
 
-export default {};
+export default {}
--- screens/StudySpacesScreen/StudySpaceResult.js
+++ screens/StudySpacesScreen/StudySpaceResult.js
@@ -1,9 +1,9 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { generate } from "shortid";
-import SearchResult from "../../components/SearchResult";
-import Colors from "../../constants/Colors";
+import React from "react"
+import PropTypes from "prop-types"
+import { connect } from "react-redux"
+import { generate } from "shortid"
+import SearchResult from "../../components/SearchResult"
+import Colors from "../../constants/Colors"
 
 const StudySpaceResult = ({
   name,
@@ -12,30 +12,30 @@ const StudySpaceResult = ({
   total,
   fetchingSeatInfo,
 }) => {
-  const occupation = occupied / total;
-  let capacityString = "Unable to get data";
-  let indicatorColor = Colors.textInputBackground;
+  const occupation = occupied / total
+  let capacityString = `Unable to get data`
+  let indicatorColor = Colors.textInputBackground
   if (total > 0) {
     switch (true) {
       case occupation > 0.9:
-        capacityString = "Very busy";
-        indicatorColor = Colors.indicatorRed;
-        break;
+        capacityString = `Very busy`
+        indicatorColor = Colors.indicatorRed
+        break
       case occupation > 0.65:
-        capacityString = "Quite busy";
-        indicatorColor = Colors.indicatorOrange;
-        break;
+        capacityString = `Quite busy`
+        indicatorColor = Colors.indicatorOrange
+        break
       case occupation > 0.5:
-        capacityString = "A little busy";
-        indicatorColor = Colors.indicatorYellow;
-        break;
+        capacityString = `A little busy`
+        indicatorColor = Colors.indicatorYellow
+        break
       case occupation > 0.2:
-        capacityString = "Rather quiet";
-        indicatorColor = Colors.indicatorLime;
-        break;
+        capacityString = `Rather quiet`
+        indicatorColor = Colors.indicatorLime
+        break
       default:
-        capacityString = "Very quiet";
-        indicatorColor = Colors.indicatorGreen;
+        capacityString = `Very quiet`
+        indicatorColor = Colors.indicatorGreen
     }
   }
   return (
@@ -50,8 +50,8 @@ const StudySpaceResult = ({
       indicatorColor={indicatorColor}
       onPress={onPress}
     />
-  );
-};
+  )
+}
 
 StudySpaceResult.propTypes = {
   onPress: PropTypes.func,
@@ -59,30 +59,30 @@ StudySpaceResult.propTypes = {
   occupied: PropTypes.number,
   total: PropTypes.number,
   fetchingSeatInfo: PropTypes.bool,
-};
+}
 
 StudySpaceResult.defaultProps = {
-  onPress: () => {},
-  name: "Study Space Name",
+  onPress: () => { },
+  name: `Study Space Name`,
   occupied: 0,
   total: 0,
   fetchingSeatInfo: false,
-};
+}
 
 const ConnectedStudySpaceResult = ({ id, studyspaces, navigation }) => {
-  const space = studyspaces.filter(x => x.id === id)[0];
+  const space = studyspaces.filter(x => x.id === id)[0]
 
   // There may be non-studyspace results e.g. 1 Saint Martin Le Grand
   // occupancy information is not available for these locations
   if (!space || space.capacity === 0) {
-    return null;
+    return null
   }
 
   return (
     <StudySpaceResult
       {...space}
       onPress={() =>
-        navigation.navigate("StudySpaceDetail", {
+        navigation.navigate(`StudySpaceDetail`, {
           id: space.id,
           name: space.name,
           occupied: space.occupied,
@@ -90,23 +90,23 @@ const ConnectedStudySpaceResult = ({ id, studyspaces, navigation }) => {
         })
       }
     />
-  );
-};
+  )
+}
 
 ConnectedStudySpaceResult.propTypes = {
   id: PropTypes.number.isRequired,
   studyspaces: PropTypes.arrayOf(PropTypes.shape()),
   navigation: PropTypes.shape().isRequired,
-};
+}
 
 ConnectedStudySpaceResult.defaultProps = {
   studyspaces: [],
-};
+}
 
 ConnectedStudySpaceResult.mapStateToProps = state => ({
   studyspaces: state.studyspaces.studyspaces,
-});
+})
 
 export default connect(ConnectedStudySpaceResult.mapStateToProps)(
   ConnectedStudySpaceResult,
-);
+)
