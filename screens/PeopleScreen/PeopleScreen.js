6a6ee268f9f43b67070ec64b83a9e193348ce692
Huey <leejinhuey@gmail.com>
2019-09-13 15:12:21 +0800

    added additional undraw illustrations

--- screens/PeopleScreen/PeopleScreen.js
+++ screens/PeopleScreen/PeopleScreen.js
@@ -1,12 +1,23 @@
 import React, { Component } from "react"
+import { Image, StyleSheet } from 'react-native'
+import { connect } from "react-redux"
 import PropTypes from "prop-types"
 import { Feather } from "@expo/vector-icons"
 import { TitleText } from "../../components/Typography"
 import { Page } from "../../components/Containers"
 import Colors from "../../constants/Colors"
+import { AssetManager } from "../../lib"
+import Styles from "../../styles/Containers"
 
 import SearchControl from "./SearchControl"
 import RecentResults from "./RecentResults"
+
+const styles = StyleSheet.create({
+  emptyImage: {
+    height: 200,
+    marginTop: 25,
+  },
+})
 
 class PeopleScreen extends Component {
   static navigationOptions = {
@@ -20,25 +31,57 @@ class PeopleScreen extends Component {
     ),
   }
 
+  static propTypes = {
+    recents: PropTypes.arrayOf(PropTypes.shape()),
+    navigation: PropTypes.shape().isRequired,
+    isSearching: PropTypes.bool,
+    searchResults: PropTypes.arrayOf(PropTypes.shape()),
+  }
+
+  static defaultProps = {
+    recents: [],
+    isSearching: false,
+    searchResults: false,
+  }
+
   static mapStateToProps = (state) => ({
+    recents: state.people.recents,
     searchResults: state.people.searchResults,
     isSearching: state.people.isSearching,
   })
 
-  static propTypes = {
-    navigation: PropTypes.shape().isRequired,
-  }
-
   render() {
-    const { navigation } = this.props
+    const {
+      navigation,
+      recents,
+      isSearching,
+      searchResults,
+    } = this.props
     return (
       <Page mainTabPage>
         <TitleText>People</TitleText>
         <SearchControl navigation={navigation} />
         <RecentResults navigation={navigation} />
+        {
+          (
+            recents.length === 0
+            && searchResults.length === 0
+            && !isSearching
+          ) ? (
+              <Image
+                source={AssetManager.undraw.peopleSearch}
+                resizeMethod="scale"
+                style={[Styles.image, styles.emptyImage]}
+                resizeMode="contain"
+              />
+            ) : null
+        }
       </Page>
     )
   }
 }
 
-export default PeopleScreen
+export default connect(
+  PeopleScreen.mapStateToProps,
+  () => ({})
+)(PeopleScreen)
--- screens/TimetableScreen/TimetableComponent.js
+++ screens/TimetableScreen/TimetableComponent.js
@@ -1,103 +1,119 @@
-import React, { Fragment } from "react";
-import { Image, View } from "react-native";
-import PropTypes from "prop-types";
-import { momentObj } from "react-moment-proptypes";
-import moment from "moment";
-import { generate } from "shortid";
-import TimetableCard from "../../components/Card/TimetableCard";
-import { CentredText, SubtitleText } from "../../components/Typography";
-import Styles from "../../styles/Containers";
+import React from "react"
+import { Image, View } from "react-native"
+import PropTypes from "prop-types"
+import { momentObj } from "react-moment-proptypes"
+import moment from "moment"
+import { generate } from "shortid"
+import TimetableCard from "../../components/Card/TimetableCard"
+import { CentredText, SubtitleText } from "../../components/Typography"
+import Styles from "../../styles/Containers"
+import { AssetManager, Random } from "../../lib"
 
-const mapToCards = (timetableItems, date, navigation, past = false) =>
-  timetableItems.map(item => (
-    <TimetableCard
-      moduleName={item.module.name}
-      moduleCode={item.module.module_id}
-      startTime={`${date} ${item.start_time}`}
-      endTime={`${date} ${item.end_time}`}
-      location={item.location.name || "TBA"}
-      lecturer={item.contact ? item.contact : "Unknown Lecturer"}
-      pastEvent={past}
-      key={generate()}
-      navigation={navigation}
-    />
-  ));
+const relaxIllustration = Random.array([
+  AssetManager.undraw.relaxation,
+  AssetManager.undraw.chilling,
+  AssetManager.undraw.playfulCat,
+  AssetManager.undraw.dogWalking,
+  AssetManager.undraw.relaxingAtHome,
+])
 
-const timetableImageStyle = { marginTop: 5, height: 200 };
-const topPadding = { height: 50 };
+const mapToCards = (
+  timetableItems,
+  date,
+  navigation,
+  past = false
+) => timetableItems.map((item) => (
+  <TimetableCard
+    moduleName={item.module.name}
+    moduleCode={item.module.module_id}
+    startTime={`${date} ${item.start_time}`}
+    endTime={`${date} ${item.end_time}`}
+    location={item.location.name || `TBA`}
+    lecturer={item.contact ? item.contact : `Unknown Lecturer`}
+    pastEvent={past}
+    key={generate()}
+    navigation={navigation}
+  />
+))
 
-const TimetableComponent = ({ timetable, date, isLoading, navigation }) => {
-  const dateISO = date.format("YYYY-MM-DD");
-  const filteredTimetable = (timetable[dateISO] || {}).timetable || [];
+const timetableImageStyle = { marginTop: 5, height: 200 }
+const topPadding = { height: 50 }
+
+const TimetableComponent = ({
+  timetable, date, isLoading, navigation,
+}) => {
+  const dateISO = date.format(`YYYY-MM-DD`)
+  const filteredTimetable = (timetable[dateISO] || {}).timetable || []
 
   if (isLoading && filteredTimetable.length === 0) {
     return (
       <View>
         <CentredText>Loading timetable...</CentredText>
       </View>
-    );
+    )
   }
 
   const items = filteredTimetable.sort(
-    (a, b) =>
-      Date.parse(`${dateISO}T${a.start_time}:00`) -
-      Date.parse(`${dateISO}T${b.start_time}:00`),
-  );
+    (a, b) => Date.parse(`${dateISO}T${a.start_time}:00`)
+      - Date.parse(`${dateISO}T${b.start_time}:00`),
+  )
   const pastItems = mapToCards(
     items.filter(
-      item => Date.parse(`${dateISO}T${item.end_time}`) - Date.now() < 0,
+      (item) => Date.parse(`${dateISO}T${item.end_time}`) - Date.now() < 0,
     ),
     dateISO,
     navigation,
     true,
-  );
+  )
   const futureItems = mapToCards(
     items.filter(
-      item => Date.parse(`${dateISO}T${item.end_time}`) - Date.now() > 0,
+      (item) => Date.parse(`${dateISO}T${item.end_time}`) - Date.now() > 0,
     ),
     dateISO,
     navigation,
-  );
+  )
   if (filteredTimetable.length > 0) {
     return (
       <View>
         {futureItems}
         {pastItems.length > 0 && (
-          <Fragment>
+          <>
             <SubtitleText>Past Events</SubtitleText>
             {pastItems}
-          </Fragment>
+          </>
         )}
       </View>
-    );
+    )
   }
   return (
     <View>
       <View style={topPadding} />
       <CentredText>
-        Nothing scheduled on {date.format("dddd")}. Take it easy!
+        Nothing scheduled on&nbsp;
+        {date.format(`dddd`)}
+        . Take it easy!
       </CentredText>
       <Image
-        source={require("../../assets/images/undraw_relaxation.png")}
+        source={relaxIllustration}
         resizeMethod="scale"
         style={[Styles.image, timetableImageStyle]}
         resizeMode="contain"
       />
     </View>
-  );
-};
+  )
+}
 
 TimetableComponent.propTypes = {
   timetable: PropTypes.shape(),
   date: momentObj,
   isLoading: PropTypes.bool,
   navigation: PropTypes.shape().isRequired,
-};
+}
 
 TimetableComponent.defaultProps = {
   timetable: {},
   date: moment(),
   isLoading: false,
-};
+}
 
-export default TimetableComponent;
+export default TimetableComponent
